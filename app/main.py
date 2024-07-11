2c75776e71c6aebeba3d3b27f6c92b9e123015bb
Stephen Adesina <bauerstevenson@gmail.com>
2024-07-11 12:11:17 +0300

    responding with a string in the body

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
+import re
 
 
 def main():
@@ -16,6 +17,11 @@ def main():
 
         if data_breadkdown[1] == "/":
             response = "HTTP/1.1 200 OK\r\n\r\n"
+
+        elif re.match(r"/[a-z]+/[a-z]+", data_breadkdown[1]):
+            param = data_breadkdown[1].split("/")[2].strip()
+            print(param)
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(param)}\r\n\r\n{param}"
 
         else:
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
