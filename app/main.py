2724c7d6166eaaf26aad0ad45c8baf583037fd54
Stephen Adesina <bauerstevenson@gmail.com>
2024-07-11 13:10:06 +0300

    Parsing the user agent header

--- app/main.py
+++ app/main.py
@@ -13,15 +13,24 @@ def main():
         client_socket, addr = server_socket.accept()
 
         data = client_socket.recv(1024)
-        data_breadkdown  = data.decode("utf-8").split(" ")
+        data_breakdown  = data.decode("utf-8").split("\r\n")
+        request_data = data_breakdown[0].split(" ")
 
-        if data_breadkdown[1] == "/":
+        if request_data[1] == "/":
             response = "HTTP/1.1 200 OK\r\n\r\n"
 
-        elif re.match(r"/[a-z]+/[a-z]+", data_breadkdown[1]):
-            param = data_breadkdown[1].split("/")[2].strip()
-            print(param)
-            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(param)}\r\n\r\n{param}"
+        elif re.match(r"/[a-z]+", request_data[1]):
+            path_data = request_data[1].split("/")
+            path = path_data[1].strip()
+
+            if path == "user-agent":
+                user_agent_string = next((s for s in data_breakdown if re.match(r"User-Agent: (.*)", s)), None)
+                user_agent = user_agent_string.split(" ")[1].strip()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+
+            if len(path_data) > 2:
+                param = path_data[2].strip()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(param)}\r\n\r\n{param}"
 
         else:
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
