bd928e40693b7bbc63a800bd01656f7fc912b3c4
Stephen Adesina <bauerstevenson@gmail.com>
2024-07-11 14:32:47 +0300

    handling concurrent requests

--- app/main.py
+++ app/main.py
@@ -1,38 +1,47 @@
 # Uncomment this to pass the first stage
 import socket
 import re
+import threading
 
+def handle_connection(client_socket, addr):
+    print(f"connection received from {addr}")
+    response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    data = client_socket.recv(1024)
+    data_breakdown  = data.decode("utf-8").split("\r\n")
+    request_data = data_breakdown[0].split(" ")
+
+    if request_data[1] == "/":
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+
+    elif re.match(r"/[a-z]+", request_data[1]):
+        path_data = request_data[1].split("/")
+        path = path_data[1].strip()
+
+        if path == "user-agent":
+            user_agent_string = next((s for s in data_breakdown if re.match(r"User-Agent: (.*)", s)), None)
+            user_agent = user_agent_string.split(" ")[1].strip()
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+
+        if len(path_data) > 2:
+            param = path_data[2].strip()
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(param)}\r\n\r\n{param}"
+
+    client_socket.send(response.encode("utf-8"))
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-    response = "HTTP/1.1 404 Not Found\r\n\r\n"
-    server_socket = socket.create_server(("localhost", 4221))
+    server_socket = socket.create_server(("localhost", 4221), backlog=5)
+    try:
+        while True:
+            client_socket, addr = server_socket.accept()
+            client_thread = threading.Thread(target=handle_connection, args=(client_socket,addr))
+            client_thread.start()
 
-    while True:
-        client_socket, addr = server_socket.accept()
-
-        data = client_socket.recv(1024)
-        data_breakdown  = data.decode("utf-8").split("\r\n")
-        request_data = data_breakdown[0].split(" ")
-
-        if request_data[1] == "/":
-            response = "HTTP/1.1 200 OK\r\n\r\n"
-
-        elif re.match(r"/[a-z]+", request_data[1]):
-            path_data = request_data[1].split("/")
-            path = path_data[1].strip()
-
-            if path == "user-agent":
-                user_agent_string = next((s for s in data_breakdown if re.match(r"User-Agent: (.*)", s)), None)
-                user_agent = user_agent_string.split(" ")[1].strip()
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-
-            if len(path_data) > 2:
-                param = path_data[2].strip()
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(param)}\r\n\r\n{param}"
-
-        client_socket.send(response.encode("utf-8"))
+    except KeyboardInterrupt:
+        print("Shutting down server")
+    
+    finally:
         server_socket.close()
 
 
